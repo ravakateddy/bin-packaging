b221935e2a81e8bfbf1f0d861e5f975d20c09ad1
ravakateddy <ravakateddy23@gmail.com>
2021-03-26 10:00:43 +0100

    test

---
+++ src/com/company/FilesTest.java
@@ -0,0 +1,30 @@
+package com.company;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class FilesTest{
+    @Test
+    public void testFiles(){
+        Path dir = Paths.get("src/com/company/data");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.txt")) {
+            for (Path file : stream) {
+                Service service = new Service();
+                //extraction des items et de la taille d'un bin à partir du fichier
+                ArrayList<Item> listItems = service.extractItemFromFile(file.toString());
+                //répartition des items dans les bins
+                ArrayList<Bin> listBins = service.fillBins(listItems);
+                System.out.println("Nombre minimum de bins pour " + file.toString() + " : " + listBins.size());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
